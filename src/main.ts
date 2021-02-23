bc5d26b64d57a8f09594d12378ebb839babbd17c
MeilCli <ics.sora.meil@gmail.com>
2021-02-23 20:44:04 +0900

    change: eslint script and refactor

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,9 @@ import * as exec from "@actions/exec";
 import * as io from "@actions/io";
 import * as os from "os";
 import * as path from "path";
-import * as fs from "fs";
 import * as process from "process";
 import { ExecOptions } from "@actions/exec/lib/interfaces";
-import {
-    OutdatedPackage,
-    Package,
-    Dependency,
-    extractJsonStringFromPostfix,
-    toPackage
-} from "./json";
+import { OutdatedPackage, Package, Dependency, extractJsonStringFromPostfix, toPackage } from "./json";
 import { getLatest } from "./semver";
 
 interface Option {
@@ -23,13 +16,13 @@ async function getOption(): Promise<Option> {
     let executeDirectories: string[] | null = core
         .getInput("execute_directories")
         .split(os.EOL)
-        .map(x => x.trim());
+        .map((x) => x.trim());
     if (executeDirectories.length == 1 && executeDirectories[0].length == 0) {
         executeDirectories = null;
     }
 
     return {
-        executeDirectories: executeDirectories
+        executeDirectories: executeDirectories,
     };
 }
 
@@ -37,9 +30,7 @@ async function checkEnvironment() {
     await io.which("swift", true);
 }
 
-async function executeOutdated(
-    executeDirectory: string | null
-): Promise<OutdatedPackage[]> {
+async function executeOutdated(executeDirectory: string | null): Promise<OutdatedPackage[]> {
     const execOption: ExecOptions = { ignoreReturnCode: true };
     if (executeDirectory != null) {
         execOption.cwd = executeDirectory;
@@ -49,18 +40,12 @@ async function executeOutdated(
     execOption.listeners = {
         stdout: (data: Buffer) => {
             stdout += data.toString();
-        }
+        },
     };
 
-    await exec.exec(
-        "swift package show-dependencies --format json",
-        undefined,
-        execOption
-    );
+    await exec.exec("swift package show-dependencies --format json", undefined, execOption);
 
-    const projectPackage: Package = toPackage(
-        extractJsonStringFromPostfix(stdout)
-    );
+    const projectPackage: Package = toPackage(extractJsonStringFromPostfix(stdout));
     const dependencies: Dependency[] = projectPackage.dependencies;
     const result: OutdatedPackage[] = [];
 
@@ -72,7 +57,7 @@ async function executeOutdated(
         childExecOption.listeners = {
             stdout: (data: Buffer) => {
                 tags += data.toString();
-            }
+            },
         };
 
         await exec.exec(`git tag`, undefined, childExecOption);
@@ -82,7 +67,7 @@ async function executeOutdated(
             result.push({
                 name: dependency.name,
                 current: dependency.version,
-                latest: latest
+                latest: latest,
             });
         }
     }
@@ -109,19 +94,16 @@ async function run() {
         const result: OutdatedPackage[] = [];
         if (option.executeDirectories == null) {
             const packages = await executeOutdated(null);
-            packages.forEach(x => result.push(x));
+            packages.forEach((x) => result.push(x));
         } else {
             for (const executeDirectory of option.executeDirectories) {
                 const packages = await executeOutdated(executeDirectory);
-                packages.forEach(x => result.push(x));
+                packages.forEach((x) => result.push(x));
             }
         }
 
         const outputText = convertToOutputText(result);
-        core.setOutput(
-            "has_swiftpm_update",
-            result.length == 0 ? "false" : "true"
-        );
+        core.setOutput("has_swiftpm_update", result.length == 0 ? "false" : "true");
         core.setOutput("swiftpm_update_text", outputText);
         core.setOutput("swiftpm_update_json", JSON.stringify(result));
     } catch (error) {
--- src/semver.ts
+++ src/semver.ts
@@ -1,12 +1,12 @@
 import * as semver from "semver";
 
 export function getLatest(current: string, versions: string): string {
-    const lines = versions.split("\n").map(x => x.trim());
+    const lines = versions.split("\n").map((x) => x.trim());
 
     let latest = current;
     for (const line of lines) {
         if (line.length == 0) {
-            continue
+            continue;
         }
 
         const version = semver.valid(line);
